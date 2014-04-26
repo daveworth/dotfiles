e14aad3bd90a9856c220a07bb289c697109ed8e0
David Worth <dworth@peakhosting.com>
2014-04-26 16:23:46 -0600

    automagically get all of the bundles for vim

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 DOTFILE_ROOT=~/.dotfiles
 
+pushd ~ > /dev/null
+
 for DIR in "vim"; do
   TARGET=~/.$DIR
   if [[ ! -e $TARGET ]]; then
@@ -26,3 +28,22 @@ if [[ ! -e $TARGET ]]; then
 else
   echo File $TARGET exists
 fi
+
+VIM_ROOT=$DOTFILE_ROOT/vim
+BUNDLE_ROOT=$VIM_ROOT/bundle
+if [[ ! -d $BUNDLE_ROOT ]]; then
+  mkdir $BUNDLE_ROOT
+fi
+pushd $BUNDLE_ROOT >> /dev/null
+while read BUNDLE_URL; do
+  BUNDLE_NAME=`echo $BUNDLE_URL | sed 's/.*\///' | sed 's/\.git$//'`
+  if [[ ! -d $BUNDLE_NAME ]]; then
+    echo $BUNDLE_NAME
+    echo $BUNDLE_URL
+    git clone $BUNDLE_URL
+  else
+    echo "$BUNDLE_NAME is already in the bundle"
+  fi
+done < $VIM_ROOT/current_bundles
+
+popd > /dev/null
